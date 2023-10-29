cb44c096dcbd781e4920ef3563ce17f680298239
filcher <146027639+filcher2011@users.noreply.github.com>
2023-10-29 21:40:47 +0300

    Add files via upload

---
+++ FakesUsers/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='FakesUsers',
+      version='1.0',
+      description='FakesUsers - Python library that regenerates random names, phone numbers and addresses. For now, only the Russian generation is available, but over time we will add other generations.',
+      packages=['FakesUsers'],
+      author_email='filcher2011@mail.ru',
+      zip_safe=False)
